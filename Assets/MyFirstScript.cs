3f811ec0781c085973a28305aab15be30fc45330
J1zuki <joycekwek61@gmail.com>
2026-04-21 11:10:07 +0800

    Initial commit

---
+++ Assets/MyFirstScript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class MyFirstScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("Hello World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // transform.position = new Vector3(
+        // transform.position.x + 0.02f, 
+        // transform.position.y, 
+        // transform.position.z
+        // );
+    }
+}
