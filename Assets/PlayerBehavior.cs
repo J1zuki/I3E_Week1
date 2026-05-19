2c1f5823d235dded4f12fa73093c994fd4f952d3
J1zuki <joycekwek61@gmail.com>
2026-05-19 14:22:15 +0800

    Week 5 UI

--- Assets/PlayerBehavior.cs
+++ Assets/PlayerBehavior.cs
@@ -1,15 +1,20 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class PlayerBehavior : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    TextMeshProUGUI scoreText;
+    public TextMeshProUGUI scoreText;
+    public Image coinTrackingIcon;
+    public Sprite[] coinTrackingSprites;
     int currentScore = 0;
+    int currentCoinCount = 0;
 
     void Start()
     {
         scoreText.text = "SCORE: " + currentScore.ToString();
+        coinTrackingIcon.sprite = coinTrackingSprites[currentCoinCount];
     }
 
     // Update is called once per frame
@@ -17,5 +22,15 @@ public class PlayerBehavior : MonoBehaviour
     {
         currentScore += amt;
         scoreText.text = "SCORE: " + currentScore.ToString();
+
+        // Update the coin tracking icon
+        ++currentCoinCount;
+
+        if (currentCoinCount >= coinTrackingSprites.Length)
+        {
+            currentCoinCount = coinTrackingSprites.Length - 1;
+        }
+
+        coinTrackingIcon.sprite = coinTrackingSprites[currentCoinCount];
     }
 }
